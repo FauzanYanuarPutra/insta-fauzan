78314809461e1031300c704637abe1df77032d3e
Fauzan <ccdplayerx@gmail.com>
2022-06-29 21:54:05 +0700

    mengupload folder untuk insta

---
+++ assets/js/app.js
@@ -0,0 +1,63 @@
+var app = new Vue({
+   el: "#app",
+   data: {
+      links: [
+         {
+            nama: "Website",
+            href: "anjay/index.html"
+         },
+         {
+            nama: "Certificate",
+            href: "certificates/certificates.html"
+         },
+         {
+            nama: "Github",
+            href: "https://github.com/Fauzanyan"
+         },
+         {
+            nama: "Contact",
+            href: "contact/contact.html"
+         }
+      ],
+      certificates: [
+         {
+            src: "../assets/img/1.jpg",
+         },
+         {
+            src: "../assets/img/2.jpg",
+         },
+         {
+            src: "../assets/img/3.jpg",
+         },
+         {
+            src: "../assets/img/4.jpg",
+         },
+         {
+            src: "../assets/img/5.jpg",
+         },
+         {
+            src: "../assets/img/6.jpg",
+         },
+         {
+            src: "../assets/img/20.jpg",
+         },
+         {
+            src: "../assets/img/21.jpg",
+         },
+         {
+            src: "../assets/img/22.jpg",
+         },
+         {
+            src: "../assets/img/23.jpg",
+         },
+         {
+            src: "../assets/img/24.jpg",
+         },
+         {
+            src: "../assets/img/25.jpg",
+         },
+      ],
+      namaDepan: "Muhammad",
+      namaBelakang: "Fauzan"
+   }
+})
